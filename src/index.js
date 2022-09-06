482a10c0312982b4e4936692267130355c875e65
Jaehyeok <milkboy2564@naver.com>
2022-09-06 22:37:40 +0900

    feat: query provider 설정
    - react-query devtools 설치
    - App 컴포넌트 query provider로 감싸기

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import { ThemeProvider } from 'styled-components';
 import App from './App';
 import theme from './styles/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
