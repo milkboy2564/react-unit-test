e79172a91c587b0998a7d2c1cb5ef21dc3909d01
Jaehyeok <milkboy2564@naver.com>
2022-09-06 18:00:20 +0900

    feat: 기본 라우팅 설정 페이지 라우팅 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 function App() {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<div>메인 페이지</div>}></Route>
+        <Route path="/movie/popular" element={<div>인기영화</div>}></Route>
+        <Route path="/movie/top_rated" element={<div>별점 높은 순</div>}></Route>
+        <Route path="/movie/upcoming" element={<div>개봉 예정 영화</div>}></Route>
+        <Route path="/movie/now_playing" element={<div>현재 상영 영화</div>}></Route>
+        <Route path="/movie/:movie_id" element={<div>영화 상세 페이지</div>}></Route>
+        <Route path="/search" element={<div>검색</div>}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
