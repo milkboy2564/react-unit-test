f4acba6e4f184a6685ab90a688ea9a6e34b1ae2b
Jaehyeok <milkboy2564@naver.com>
2022-09-06 23:13:05 +0900

    style: 안쓰는 컴포넌트 제거

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import GlobalStyle from './styles/Globalstyles';
-import styled from 'styled-components';
 
 function App() {
   return (
     <Router>
-      <Container>
-        <GlobalStyle />
-        <Routes>
-          <Route path="/" element={<div>메인dsgsdgds 페이지</div>}></Route>
-          <Route path="/movie/popular" element={<div>인기영화</div>}></Route>
-          <Route path="/movie/top_rated" element={<div>별점 높은 순</div>}></Route>
-          <Route path="/movie/upcoming" element={<div>개봉 예정 영화</div>}></Route>
-          <Route path="/movie/now_playing" element={<div>현재 상영 영화</div>}></Route>
-          <Route path="/movie/:movie_id" element={<div>영화 상세 페이지</div>}></Route>
-          <Route path="/search" element={<div>검색</div>}></Route>
-        </Routes>
-      </Container>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<div>메인dsgsdgds 페이지</div>}></Route>
+        <Route path="/movie/popular" element={<div>인기영화</div>}></Route>
+        <Route path="/movie/top_rated" element={<div>별점 높은 순</div>}></Route>
+        <Route path="/movie/upcoming" element={<div>개봉 예정 영화</div>}></Route>
+        <Route path="/movie/now_playing" element={<div>현재 상영 영화</div>}></Route>
+        <Route path="/movie/:movie_id" element={<div>영화 상세 페이지</div>}></Route>
+        <Route path="/search" element={<div>검색</div>}></Route>
+      </Routes>
     </Router>
   );
 }
 
-const Container = styled.div`
-  width: 1190px;
-  margin: 0 auto;
-`;
 export default App;
