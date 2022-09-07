f9ca8730308d5e0768f6e2afa4a1b3dd474b56ce
Jaehyeok <milkboy2564@naver.com>
2022-09-07 20:56:48 +0900

    fix: 검색 페이지 conflict 해결
    Route 컴포넌트 변경에 따른 충돌 해결

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MovieDetail from './pages/movieDetail/MovieDetail';
 import Header from './components/Header';
 import Search from './pages/search/Search';
 import GlobalStyle from './styles/Globalstyles';
@@ -14,7 +15,7 @@ function App() {
         <Route path="/movie/top_rated" element={<div>별점 높은 순</div>}></Route>
         <Route path="/movie/upcoming" element={<div>개봉 예정 영화</div>}></Route>
         <Route path="/movie/now_playing" element={<div>현재 상영 영화</div>}></Route>
-        <Route path="/movie/:movie_id" element={<div>영화 상세 페이지</div>}></Route>
+        <Route path="/movie/:movie_id" element={<MovieDetail />}></Route>
         <Route path="/search" element={<Search />}></Route>
       </Routes>
     </Router>
---
+++ src/pages/movieDetail/components/subTab/TabCast.jsx
@@ -0,0 +1,31 @@
+import * as S from './SubTab.styles';
+
+const TabCast = ({ creditData, url, handleOnErrorImage }) => {
+  const { cast } = creditData;
+  const MAIN_CAST = cast.slice(0, 9);
+
+  return (
+    <>
+      <S.Section>
+        <S.SectionTitle>출연</S.SectionTitle>
+        <S.SectionContent>
+          <S.Cast>
+            {MAIN_CAST.map(figure => (
+              <S.CastCard key={figure.cast_id}>
+                <S.CastImage>
+                  <img src={url + figure.profile_path} alt="figure" onError={handleOnErrorImage} />
+                </S.CastImage>
+                <S.CastInfo>
+                  <S.Name>{figure.name}</S.Name>
+                  <S.Character>{figure.character} 역</S.Character>
+                </S.CastInfo>
+              </S.CastCard>
+            ))}
+          </S.Cast>
+        </S.SectionContent>
+      </S.Section>
+    </>
+  );
+};
+
+export default TabCast;
