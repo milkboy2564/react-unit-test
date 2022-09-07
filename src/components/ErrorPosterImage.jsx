fc425984db9b19920e9b49efe8c30d38541fc7ba
wisdoom03 <wisdoom03@gmail.com>
2022-09-08 01:56:52 +0900

    chore: 불필요한 파일 삭제 ErrorImage 컴포넌트 삭제

--- src/components/ErrorPosterImage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styled from 'styled-components';
-const ErrorPosterImage = () => {
-  return <ReplaceImage src={process.env.PUBLIC_URL + '/errImage.png'} alt="포스터가 없습니다." />;
-};
-const ReplaceImage = styled.img`
-  width: 100%;
-`;
-export default ErrorPosterImage;
