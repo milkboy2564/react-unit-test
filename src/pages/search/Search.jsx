404aa1420c8e2b55184b656cbf472985e1b1c660
Jaehyeok <milkboy2564@naver.com>
2022-09-07 17:57:05 +0900

    fix: API Request 변경 react-query 사용하여 서버 데이터 캐싱

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,28 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import SearchMovieCard from './components/SearchMovieCard';
 import { searchApi } from '../../services/api';
 import { useLocation } from 'react-router-dom';
+import { useQuery } from 'react-query';
 
 function Search() {
   const location = useLocation();
-  const [movies, setMovies] = useState([]);
-  const getSearchResult = async () => {
-    try {
-      const {
-        data: { results },
-      } = await searchApi.movies(decodeURI(location.search.slice(3)));
-      setMovies(results);
-    } catch (e) {
-      alert('검색 결과 에러');
+  const searchWord = location.search.slice(3);
+  const { data: movieResult } = useQuery(
+    ['search', searchWord],
+    () => searchApi.movies(decodeURI(searchWord)),
+    {
+      enabled: !!searchWord,
+      refetchOnWindowFocus: false,
+      staleTime: 180000,
     }
-  };
-  useEffect(() => {
-    getSearchResult();
-  }, [location]);
+  );
 
   return (
     <>
-      {movies.map(movie => (
+      {movieResult?.data?.results.map(movie => (
         <SearchMovieCard key={movie.id} movie={movie}></SearchMovieCard>
       ))}
     </>
