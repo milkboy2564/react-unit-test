12eaaf77ff67fdc283abbc172f0711794c99f6db
Jaehyeok <milkboy2564@naver.com>
2022-09-08 00:17:58 +0900

    feat: 위로가기 버튼 Layout에서 관리 모든 페이지에서 관리할 수 있도록 Layout component에서 관리

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,17 +1,51 @@
 import React, { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
+import styled from 'styled-components';
 import Header from './Header';
 import Loading from './Loading';
 
 function Layout() {
+  const onUpClick = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+  };
   return (
     <>
       <Header />
       <Suspense fallback={<Loading />}>
         <Outlet />
+        <UpButton onClick={onUpClick}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 19.5v-15m0 0l-6.75 6.75M12 4.5l6.75 6.75"
+            />
+          </svg>
+        </UpButton>
       </Suspense>
     </>
   );
 }
+
+const UpButton = styled.button`
+  position: fixed;
+  right: 20px;
+  bottom: 20px;
+  width: 60px;
+  height: 60px;
+  background-color: rgba(15, 15, 15, 0.6);
+  color: white;
+  border-radius: 50%;
+  &:active {
+    transform: scale(0.98);
+  }
+`;
 
 export default Layout;
--- src/pages/movieList/MoviePage.jsx
+++ src/pages/movieList/MoviePage.jsx
@@ -25,31 +25,11 @@ function MoviePage() {
     }
   });
 
-  const onUpClick = () => {
-    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
-  };
-
   return (
     <PageContainer>
       {isSuccess &&
         pageDatas.map((item, index) => <Movie key={item.id} movie={item} rank={index + 1} />)}
       <Target ref={ref} />
-      <UpButton onClick={onUpClick}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M12 19.5v-15m0 0l-6.75 6.75M12 4.5l6.75 6.75"
-          />
-        </svg>
-      </UpButton>
     </PageContainer>
   );
 }
@@ -63,20 +43,6 @@ const PageContainer = styled.div`
 
 const Target = styled.div`
   height: 1px;
-`;
-
-const UpButton = styled.button`
-  position: fixed;
-  right: 20px;
-  bottom: 20px;
-  width: 60px;
-  height: 60px;
-  background-color: rgba(15, 15, 15, 0.6);
-  color: white;
-  border-radius: 50%;
-  &:active {
-    transform: scale(0.98);
-  }
 `;
 
 export default MoviePage;
