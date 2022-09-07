d8726961a1bd6357eb0abb226e319001c22db28c
Jaehyeok <milkboy2564@naver.com>
2022-09-07 23:06:17 +0900

    feat: Loading Spinner 적용 Suspense 컴포넌트 이용하여 최상단에서 비동기 요청 시간 동안 Loading Spinner 보여줄 수 있게끔 구현

--- src/App.js
+++ src/App.js
@@ -2,25 +2,29 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import UpComing from './pages/upComing/UpComing';
 import TopRated from './pages/topRated/TopRated';
 import MovieDetail from './pages/movieDetail/MovieDetail';
-import Header from './components/Header';
 import Search from './pages/search/Search';
 import NowPlaying from './pages/nowPlaying/NowPlaying';
 import GlobalStyle from './styles/Globalstyles';
+import Header from './components/Header';
+import { Suspense } from 'react';
+import Loading from './components/Loading';
 
 function App() {
   return (
     <Router>
       <GlobalStyle />
       <Header />
-      <Routes>
-        <Route path="/" element={<div>메인dsgsdgds 페이지</div>}></Route>
-        <Route path="/movie/popular" element={<div>인기영화</div>}></Route>
-        <Route path="/movie/upcoming" element={<UpComing />}></Route>
-        <Route path="/movie/top_rated" element={<TopRated />}></Route>
-        <Route path="/movie/:movie_id" element={<MovieDetail />}></Route>
-        <Route path="/search" element={<Search />}></Route>
-        <Route path="/movie/now_playing" element={<NowPlaying />}></Route>
-      </Routes>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/" element={<div>메인dsgsdgds 페이지</div>}></Route>
+          <Route path="/movie/popular" element={<div>인기영화</div>}></Route>
+          <Route path="/movie/upcoming" element={<UpComing />}></Route>
+          <Route path="/movie/top_rated" element={<TopRated />}></Route>
+          <Route path="/movie/:movie_id" element={<MovieDetail />}></Route>
+          <Route path="/search" element={<Search />}></Route>
+          <Route path="/movie/now_playing" element={<NowPlaying />}></Route>
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
