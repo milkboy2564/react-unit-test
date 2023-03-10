33843b62c8220b7d7711d6c6f967a954acb3f9a4
SeolJaehyeok <milkboy2564@naver.com>
2023-03-10 14:21:25 +0900

    Husky Test

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ import Layout from './components/Layout';
 import NotFound from './components/NotFound';
 import MoviePage from './pages/movieList/MoviePage';
 
-// Husky Test
-
 function App() {
   return (
     <Router>
